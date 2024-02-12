462f2c9528aee97a55a517eff361e2bd960842ca
John Todd <jqt3of@gmail.com>
2024-02-12 15:04:28 -0700

    Initial commit

---
+++ TestProject1/UnitTest1.cs
@@ -0,0 +1,211 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using WordWallGenerator;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestTree()
+        {
+            var nodes = new List<string>() { "I love Kira", "I love you" }
+                .Select(line => line.Split(" "))
+                .ToDigraph();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes.Select(n => n.Value), Contains.Item("I") );
+            
+        }
+        
+        [Test]
+        public void TestTree1()
+        {
+            var nodes = new List<string>() { "I love Kira", "I hate you" }
+                .Select(line => line.Split(" "))
+                .ToDigraph();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes.Select(n => n.Value), Contains.Item("I") );
+            
+        }
+        
+        [Test]
+        public void TestTree2()
+        {
+            var nodes = new List<string>() { "I love Kira", "we hate you" }
+                .Select(line => line.Split(" "))
+                .ToDigraph();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes.Select(n => n.Value), Contains.Item("I") );
+            Assert.That(nodes.Select(n => n.Value), Contains.Item("we") );
+            
+        }
+        
+        [Test]
+        public void TestTree3()
+        {
+            var nodes = new List<string>() { "I love Kira", "we hate you",  "I love you"  }
+                .Select(line => line.Split(" "))
+                .ToDigraph();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes.Select(n => n.Value), Contains.Item("I") );
+            Assert.That(nodes.Select(n => n.Value), Contains.Item("we") );
+            
+        }
+        
+        [Test]
+        public void TestTree4()
+        {
+            var nodes = new List<string>() { "I love Kira", "love conquers all", "conquers her land" }
+                .Select(line => line.Split(" "))
+                .ToDigraph();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes.Select(n => n.Value), Contains.Item("I") );
+            
+        }
+        
+        [Test]
+        public void TestTree5()
+        {
+            var nodes = new List<string>() { "love conquers all", "I love Kira", "conquers her land" }
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            
+        }
+        
+        [Test]
+        public void TestCycle()
+        {
+            var nodes = new List<string>() { "JT loves Kira", "Kira loves JT" }
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Select(n => n.Value);
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes, Contains.Item("JT") );
+        }
+        
+        [Test]
+        public void TestCycleFlatten()
+        {
+            var nodes = new List<string>() { "JT loves Kira", "Kira loves JT" }
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes, Is.EquivalentTo(new []{"JT", "loves", "Kira", "loves2", "JT2"})); 
+        }
+        
+        [Test]
+        public void TestCycleFlattenReverse()
+        {
+            var nodes = new List<string>() { "Kira loves JT",  "JT loves Kira", }
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes, Is.EquivalentTo(new []{"Kira", "loves", "JT", "loves2", "Kira2"})); 
+        }
+        
+        [Test]
+        public void TestCycleFlatten2()
+        {
+            var nodes = new List<string>() { "JT loves his Kira", "his Kira really wants to like JT" }
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes, Is.EquivalentTo(new []{"JT", "loves", "his", "Kira", "really", "wants", "to", "like", "JT2"})); 
+        }
+        
+        [Test]
+        public void TestFlattenTree3()
+        {
+            var nodes = new List<string>() { "I love Kira", "we hate you",  "I love you"  }
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+            
+            Assert.That(nodes, Is.Not.Empty);
+            Assert.That(nodes, Contains.Item("I") );
+            Assert.That(nodes, Contains.Item("we") );
+            Assert.That(nodes, Contains.Item("Kira") );
+            Assert.That(nodes, Contains.Item("hate") );
+            Assert.That(nodes, Contains.Item("love") );
+            Assert.That(nodes, Contains.Item("you") );
+            
+        }
+
+        [Test]
+        public void TestSentances()
+        {
+            var digraph = Program.ReadLines("sentances.txt")
+                //tokenize
+                .Select(line => line.Split(" "))
+                .ToDigraph();
+            var str = digraph.Flatten();
+
+            var tokens = Program.ReadLines("sentances.txt")
+                //tokenize
+                .Select(line => line.Split(" "));
+
+            foreach (var line in tokens)
+            {
+                int index = 0;
+                foreach (var token in line)
+                {
+                   int i = str.Skip(index).ToList().IndexOf(token);
+                   if (i == -1)
+                   {
+                       i = str.Skip(index).ToList().IndexOf(token +"2");
+                       if (i == -1)
+                       {
+                           i = str.Skip(index).ToList().IndexOf(token +"3");
+                       }
+                   }
+                   Assert.That(i, Is.GreaterThan(-1), $"can't find word {token}");
+                   index = i + index;
+                }
+            }
+        }
+        
+        [Test]
+        public void TestCycleFlatten3()
+        {
+            var nodes = new List<string>() { "JT loves his awesome Kira", "what loves a really super awesome dude" }
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+            
+            Assert.That(nodes.ToArray(), Is.Not.Empty);
+        }
+        
+        [Test]
+        public void TestCycleFlatten4()
+        {
+            var nodes = new List<string>() { "A B C D", "X Y C A", "H I C", "Z U I", "P O H"}
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+            
+            Assert.That(nodes.ToArray(), Is.Not.Empty);
+        }
+    }
+}
---
+++ WordWallGenerator/Program.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace WordWallGenerator
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public HashSet<Node> Edges { get; } = new();
+        public HashSet<Node> BackEdges { get; } = new();
+
+        public override int GetHashCode()
+        {
+            return Value.ToLower().GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Node node)
+            {
+                return Value.ToLower().Equals(node.Value.ToLower());
+            }
+
+            return false;
+        }
+
+        public override string ToString()
+        {
+            return Value;
+        }
+    }
+
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            //Construct a multi-root acyclic digraph with unique nodes. 
+            //This structure is then traversed in a breadth-first fashion to ensure every sentence is representable 
+            
+            //TODO: prefix words.... don't need cook and cooks as separate words
+            
+            var nodes = ReadLines("sentances.txt")
+                //tokenize
+                .Select(line => line.Split(" "))
+                .ToDigraph()
+                .Flatten();
+
+            var builder = new StringBuilder();
+            int lineLength = 0;
+            foreach (var node in nodes)
+            {
+                lineLength += node.Length;
+                if (lineLength > 34)
+                {
+                    builder.Append("\n");
+                    lineLength = node.Length;
+                }
+                builder.Append(node);
+            }
+
+            var str = builder.ToString();
+            Console.WriteLine(str);
+            Console.WriteLine("count: " + str.Length);
+        }
+
+        public static HashSet<Node> ToDigraph(this IEnumerable<string[]> matrix)
+        {
+            //For quick access to each node 
+            var nodes = new Dictionary<string, Node>();
+
+            Node AddNewNode(string token)
+            {
+                var node = new Node()
+                {
+                    Value = token 
+                };
+                        
+                nodes.Add(token.ToLower(), node);
+
+                return node;
+            }
+
+            bool TryGetNode(string token, out Node? node)
+            {
+                return nodes.TryGetValue(token.ToLower(), out node);
+            }
+
+            bool IsNodeInPath(Node child, Node node)
+            {
+                if (child == node)
+                {
+                    return true;
+                }
+                if (!child.BackEdges.Any())
+                {
+                    return false;
+                }
+
+                foreach (var back in child.BackEdges)
+                {
+                    if (IsNodeInPath(back, node))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+            
+            
+            //Make sure we identify the root nodes
+            var roots = new HashSet<Node>();
+                
+            foreach (var line in matrix)
+            {
+                Node? node = null;
+                var token = line[0];
+                //Haven't seen this word yet, initialize the edge list
+                if (!TryGetNode(token, out node))
+                {
+                    node = AddNewNode(token);
+                        
+                    //each of the first words in a sentence might be a root word....
+                    roots.Add(node);
+                } 
+                
+                for (var i = 1; i < line.Length; ++i)
+                {
+                    //grab the next node and add an edge to the edge list if there is one
+                   
+                    var next = line[i];
+                    //Haven't seen this word yet, initialize the edge list
+                    if (!TryGetNode(next, out var node2))
+                    {
+                        node2 = AddNewNode(next);
+                    }
+
+                    //We want to be careful about cycles.... if adding this node will create a cycle, we're going to create a new node instead
+                    var index = 2;
+                    while (IsNodeInPath(node, node2))
+                    {
+                        //Does the node exist already?
+                        if (!TryGetNode(next + index, out node2))
+                        {
+                            //If not, create it
+                            node2 = AddNewNode(next + index);
+                        }
+
+                        index += 1;
+                    }
+                   
+                    //Add the edge
+                    node.Edges.Add(node2);
+                    node2.BackEdges.Add(node);
+
+                    node = node2;
+                }
+            }
+
+            return roots;
+        }
+        public static IEnumerable<string> Flatten(this IEnumerable<Node> roots)
+        {
+            //Breadth  first ordered traversal of a multi-root acyclic digraph
+            var queue = new Queue<Node>();
+
+            var visitCount = new Dictionary<Node, int>();
+            
+            //Enqueue the roots first
+            foreach (var root in roots)
+            {
+                queue.Enqueue(root);
+                //Enqueuing a root here DOES NOT count as a visit. Sometimes roots have backedges, and those should not be processed immediately
+                visitCount[root] = -1;
+            }
+            
+            while (queue.Count > 0)
+            {
+                var word = queue.Dequeue();
+
+                //nodes might have multiple parents, and we don't want to output them until all the parentpaths have been traversed
+                //so keep count 
+                if (!visitCount.TryGetValue(word, out int count))
+                {
+                    visitCount[word] = 0;
+                }
+                
+                visitCount[word] = count + 1;
+                if (visitCount[word] < word.BackEdges.Count)
+                {
+                    continue;
+                }
+
+                yield return word.Value;
+
+                //Naively no sorting
+                foreach (var next in word.Edges)
+                {
+                    queue.Enqueue(next);
+                }
+            } 
+        }
+        public static IEnumerable<string> ReadLines(string filename)
+        {
+            using (var file = File.OpenText(filename))
+            {
+                string line = string.Empty;
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (!string.IsNullOrEmpty(line))
+                    {
+                        yield return line;
+                    }
+                }
+            } 
+        }
+
+    }
+}
