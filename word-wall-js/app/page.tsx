dfcbde2ac55deee015c31d7e5f1c121589987a28
jqt3of5 <jqt3of5@gmail.com>
2025-10-26 20:36:17 -0600

    Added the web app to the repo

---
+++ word-wall-js/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import WordWall from "@/app/WordWall";
+
+export default function Home() {
+  return (
+      <main>
+          <WordWall></WordWall>
+      </main>
+  );
+}
